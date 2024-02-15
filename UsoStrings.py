888a2fccb9f7cdddeb484c6cd97c4eefd609e524
tacchy <ttacchy@gmail.com>
2024-02-15 11:21:18 -0300

    versão 1

---
+++ UsoStrings.py
@@ -0,0 +1,7 @@
+# coding: utf-8
+print("""\
+Uso: consulta_base [OPCOES]
+     -h               Exibe saída de ajuda
+     -U               url Url do dataset
+""")
+#Repare na \ no início da string que escapa o \n da quebra de linha.
